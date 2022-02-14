5623339bb0fbe49aa161f06db7495baa4a681963
Ni4ka1991 <astahova_nica@mail.ru>
2022-02-14 12:55:33 +0200

    Add data

--- app.py
+++ app.py
@@ -1,45 +1,62 @@
 #!/usr/bin/env python3
+
 from PIL import Image, ImageOps
 import matplotlib.pyplot as plt
 import torch, torch.nn as nn
-from torchvision import transforms
+from torchvision import transforms, datasets
 import numpy
 import sys
 
-#numpy.set_printoptions( threshold = sys.maxsize )
-
-img = Image.open( "./data/train/good_pear/9.jpg" )
-print( "This is size of original image: ", img.size )
-
-crop = img.resize(( 128, 128 ), Image.ANTIALIAS )
-gray = ImageOps.grayscale( crop )
-print( "This is size of GrayscaleCrop image: ", gray.size, "\n" )
-
-
-#VIEW DATA
-
-#plt.figure()
-#plt.imshow( img, interpolation = "nearest" )
-#plt.figure()
-#plt.imshow( gray, interpolation = "nearest", cmap = "gray" )
-#plt.show()
+#set all transforms on a dataset
+transform_train = transforms.Compose([
+                                   transforms.Grayscale(),
+                                   transforms.RandomRotation( 10 ),
+                                   transforms.Resize( 150 ),
+                                   transforms.CenterCrop( 128 ),
+                                   transforms.RandomHorizontalFlip(),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize([0.5], [0.5])
+])
 
 
+dataset_train = datasets.ImageFolder( "data/train", transform = transform_train )
+dataloader_train = torch.utils.data.DataLoader( dataset_train, batch_size = 32, shuffle = True )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 #Create a single neuron
 
-#cnn = nn.Conv2d( 128, 128, 3 )
-cnn = nn.Conv2d( 1, 1, 3 )                                            # create one 3/3 neuron for gray scale images
-neuron_weights = cnn.weight.data.numpy()                                   #convert to numpy data for visualization
-print( f"Weights in a single 3/3 neuron >>>\n {neuron_weights} " )
+#cnn = nn.Conv2d( 1, 8, 5 )                                                 # create one 5/5 neuron for gray scale images and eight(8) outputs
+#neuron_weights = cnn.weight.data.numpy()                                   # convert to numpy data for visualization
+#print( f"Weights in a single 5/5 neuron >>>\n {neuron_weights} " )
 
 #Transform data to tensor
-img_tensor = transforms.ToTensor()( gray ).unsqueeze_( 0 )
+#img_tensor = transforms.ToTensor()( gray ).unsqueeze_( 0 )
 
 #Update tensor from created neuron
-filtered_img_tensor = cnn( img_tensor )
+#filtered_img_tensor = cnn( img_tensor )
+
 
 
 #View tensors and his properties (shape)
+
 print( )
 print( f"Image to tensor print>>>\n {img_tensor}" )
 print( "The end of image tensor" )
@@ -56,9 +73,9 @@ print( "The end" )
 filtered_img = transforms.ToPILImage()( filtered_img_tensor.squeeze_( 0 ))
 
 #view filtered img  
-plt.figure()
-plt.imshow( filtered_img, interpolation = "nearest", cmap = "gray" )
-plt.show()
-
+#plt.figure()
+#plt.imshow( filtered_img, interpolation = "nearest", cmap = "gray" )
+#plt.show()
+"""
 
 
