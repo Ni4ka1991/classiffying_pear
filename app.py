2fbc24d46424907c3fdb6f0672af667f6060d1b5
astahova veronica <veronika@Veronika-notebook.localdomain>
2022-02-24 15:42:20 +0200

    print dataloader_

--- app.py
+++ app.py
@@ -16,34 +16,36 @@ import sys
 #apply transforms regulations
 dataset_train =    datasets.ImageFolder( "data/train", transform = transform_train )
 
-dataloader_train = utils.data.DataLoader( dataset_train, batch_size = 32, shuffle = True )
+dataloader_train = utils.data.DataLoader( dataset_train, batch_size = 2, shuffle = True )
 
 #print( model )
 
-print( dataloader_train )
+#print( dataloader_train )
 print( len( dataloader_train ))
+
 input( "hit enter ..." )
 
-for epoch in range( 3 ):
-    running_loss = 0.0
 
-    for i, data in enumerate( dataloader_train, 0 ):
+#for epoch in range( 3 ):
+#    running_loss = 0.0
 
-        print( "i    >>>> {}".format( i ))
+#    for i, data in enumerate( dataloader_train, 0 ):
+
+#        print( "i    >>>> {}".format( i ))
 #        print( "data >>>> {}".format( data ))
-"""
-        inputs, labels = data
-        optimizer.zero_grad()
-        outputs = model( inputs )
-        loss = criterion( outputs, labels )
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
 
-if epoch % 1 == 0:
-    print( "Epoch: {} \t Training loss: {:.6f}".format( epoch, running_loss ) )
 
-"""
+#        inputs, labels = data
+#        optimizer.zero_grad()
+#        outputs = model( inputs )
+#        loss = criterion( outputs, labels )
+#        loss.backward()
+#        optimizer.step()
+#        running_loss += loss.item()
+
+#if epoch % 1 == 0:
+#    print( "Epoch: {} \t Training loss: {:.6f}".format( epoch, running_loss ) )
+
 
 
 
