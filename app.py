1a6953339f23c349fe111ae43df6a89bc4048926
astahova veronica <veronika@Veronika-notebook.localdomain>
2022-03-09 11:51:18 +0200

    Some changes

--- app.py
+++ app.py
@@ -15,14 +15,14 @@ import sys
 
 #apply transforms regulations
 dataset_train =    datasets.ImageFolder( "data/train", transform = transform_train )
-print( f"type( dataset_train ) >>> {type( dataset_train )}" )
-print( f"Dataset_len ( data/train ) >>> {len( dataset_train )}" )
-print(f"\ndataset_getitem[15] >>>\n{dataset_train[15]}")
-print(f"\ndataset_getitem[16] >>>\n{dataset_train[16]}")
-print(f"\ndataset_getitem[16][0] >>>\n{dataset_train[16][0]}")
-print(f"\ndataset_getitem[16][1] >>>\n{dataset_train[16][1]}")
-dataset_train_16 = np.array( dataset_train[16][0] )
-print(f"\nShape of dataset_getitem[16][0] >>>  {dataset_train_16.shape}")
+#print( f"type( dataset_train ) >>> {type( dataset_train )}" )
+#print( f"Dataset_len ( data/train ) >>> {len( dataset_train )}" )
+#print(f"\ndataset_getitem[15] >>>\n{dataset_train[15]}")
+#print(f"\ndataset_getitem[16] >>>\n{dataset_train[16]}")
+#print(f"\ndataset_getitem[16][0] >>>\n{dataset_train[16][0]}")
+#print(f"\ndataset_getitem[16][1] >>>\n{dataset_train[16][1]}")
+#dataset_train_16 = np.array( dataset_train[16][0] )
+#print(f"\nShape of dataset_getitem[16][0] >>>  {dataset_train_16.shape}")
 #input( "hit enter ..." )
 
 dataloader_train = utils.data.DataLoader( dataset_train, batch_size = 1, shuffle = True )
@@ -31,18 +31,23 @@ dataloader_train = utils.data.DataLoader( dataset_train, batch_size = 1, shuffle
 
 #print( dataloader_train )
 #print( len( dataloader_train ))
-print( f"\nLen of dataloader_train >>>{len( dataloader_train )}" )
-print( f"\ntype( dataloader_train )  >>>{type( dataloader_train )}" )
+#print( f"\nLen of dataloader_train >>>{len( dataloader_train )}" )
+#print( f"\ntype( dataloader_train )  >>>{type( dataloader_train )}" )
 
-for i, data in enumerate( dataloader_train, 0 ):
-    inputs, labels = data
-    optimizer.zero_grad()
-    outputs = model( inputs )
-    loss = criterion( outputs, labels )
-    loss.backward()
-    optimizer.step()
-    running_loss += loss.item()
+for epoch in range( 3 ):
+    running_loss = 0.0
+    
+    for i, data in enumerate( dataloader_train, 0 ):
+        inputs, labels = data
+        optimizer.zero_grad()
+        outputs = model( inputs )
+        loss = criterion( outputs, labels )
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
 
+if epoch %1 == 0:
+    print( "Epoch: {} \tTraining Loss: {:.6f}".format( epoch, running_loss ))
 
 
 
