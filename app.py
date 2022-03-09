178bed2650c0be6536cc1d9dfd9ac699069e04ee
Ni4ka1991 <astahova_nica@mail.ru>
2022-03-09 12:27:13 +0200

    Data reduction

--- app.py
+++ app.py
@@ -14,29 +14,10 @@ import numpy as np
 import sys
 
 #apply transforms regulations
-dataset_train =    datasets.ImageFolder( "data/train", transform = transform_train )
-print( f"type( dataset_train ) >>> {type( dataset_train )}" )
-print( f"Dataset_len ( data/train ) >>> {len( dataset_train )}" )
-print(f"\ndataset_getitem[15] >>>\n{dataset_train[15]}")
-print(f"\ndataset_getitem[16] >>>\n{dataset_train[16]}")
-print(f"\ndataset_getitem[16][0] >>>\n{dataset_train[16][0]}")
-print(f"\ndataset_getitem[16][1] >>>\n{dataset_train[16][1]}")
-dataset_train_16 = np.array( dataset_train[16][0] )
-print( f"type( dataset_getitem[16][0] ) >>> {type( dataset_train[16][0] )}" )
-print(f"\nShape of dataset_getitem[16][0] >>>  {dataset_train_16.shape}")
-#print(f"\nAxis of dataset_getitem[16][0] >>>  {dataset_train_16.dim}")
-#input( "hit enter ..." )
+dataset_train =    datasets.ImageFolder( "sml_data/train", transform = transform_train )
+dataloader_train = utils.data.DataLoader( dataset_train, batch_size = 1, shuffle = True )
 
-dataloader_train = utils.data.DataLoader( dataset_train, batch_size = 3, shuffle = True )
-
-#print( model )
-
-print( f"\nLen of dataloader_train >>>{len( dataloader_train )}" )
-#print( f"\nIf batch_size = 2, то тенсоры объединяются по два и соответственно длина dataloader_train сокращается в 2. dataloader_train = dataset/batch_size" )
-print( f"\ntype( dataloader_train )  >>>{type( dataloader_train )}" )
-
-
-input( "hit enter ..." )
+print( model )
 
 
 #for epoch in range( 3 ):
